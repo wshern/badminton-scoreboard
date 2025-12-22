e44db9dd86ed9c7d6dd5632d22acf9c634fc25a3
wslim <imeva.ws@gmail.com>
2025-12-22 17:54:42 +0800

    writing js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-function startGame() {
-    console.log("Game started!");
-}
+const player1NameInput = document.getElementById("player1-name-input");
+const player2NameInput = document.getElementById("player2-name-input");
+const startGameBtn = document.getElementById("start-game-btn");
+const scoreboardSection = document.getElementById("scoreboard");
